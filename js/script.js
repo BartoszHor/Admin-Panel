1504b342c21d10d28073f7a5878b3fe8d1440d52
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-09-07 11:57:13 +0200

    Add banners list and clickhandlers for links to switch between main contents

--- js/script.js
+++ js/script.js
@@ -19,13 +19,25 @@ for(let datePicker of datePickers){
   datePicker.value = formatDate(new Date());
 }
 
+function activatePage() {
+  let mainPages = document.querySelectorAll('.main-wrapper')
+  for (let page of mainPages) {
+    if (page.id == 1) {
+      page.classList.add('active')
+    }
+  }
+}
+activatePage()
+
+/* Sidebar functions */
+
 function openCloseSidebar() {
   let sidebar = document.querySelector('.sidebar');
   if (sidebar.style.display == 'block') {
-    sidebar.style.display = 'none'
-    let size = document.body.clientWidth
+    sidebar.style.display = 'none';
+    let size = document.body.clientWidth;
     if (size >= 737) {
-      sidebar.style.display = 'block'
+      sidebar.style.display = 'block';
     }
   } else {
     sidebar.style.display = 'block';
@@ -36,14 +48,30 @@ function showSidebar() {
   let size = document.body.clientWidth;
   if (size >= 737) {
     let sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'block'
+    sidebar.style.display = 'block';
   }
-
 }
-
 
 const sidebarIcon = document.querySelector('.icon-arrow-right');
 sidebarIcon.addEventListener('click', openCloseSidebar);
 
 const closeIcon = document.querySelector('.icon-close');
 closeIcon.addEventListener('click', openCloseSidebar);
+
+
+function pickSection(event) {
+event.preventDefault();
+const clickedElement = this
+let mainSections = document.querySelectorAll('.main-wrapper')
+for (let section of mainSections) {
+    section.classList.toggle('active', section.id == clickedElement.id)
+  }
+}
+
+function sidebarListeners() {
+  let sidebarLinks = document.querySelectorAll('.sidebar__list__item a')
+  for (let link of sidebarLinks) {
+    link.addEventListener('click', pickSection)
+  }
+}
+sidebarListeners()
