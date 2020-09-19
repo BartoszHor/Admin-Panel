cf38b4cb3d7ebfbb4354817241229fd97ea44d3c
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-09-19 19:52:37 +0200

    Add chart from chart.js and modal with pop up and hide functions

--- js/script.js
+++ js/script.js
@@ -42,6 +42,43 @@ function initPage() {
 }
 initPage();
 
+/* Modal functions */
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  })
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal()
+  }
+})
+
+document.addEventListener('keyup', function(e) {
+  if (e.key == 'Escape') {
+    closeModal()
+  }
+})
+
+function modalOpener(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show')
+  })
+  document.querySelector('#overlay').classList.add('show')
+  document.querySelector(modal).classList.add('show')
+}
+
+document.querySelector('.sidebar-manager').addEventListener('click', function(){
+  modalOpener('#myModal')
+})
+
 
 /* Sidebar functions */
 
@@ -64,6 +101,7 @@ function showSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'block';
   }
+
 }
 
 const sidebarIcon = document.querySelector('.icon-arrow-right');
@@ -90,3 +128,54 @@ function sidebarListeners() {
   }
 }
 sidebarListeners();
+
+const searchInput = document.querySelector('.search-input')
+searchInput.addEventListener('focus', function() {
+document.querySelector('.icon-message').classList.add('hide')
+})
+
+/* Chart */
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        datasets: [{
+            label: "Signups",
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+
+        }]
+    },
+    options: {
+        layout: {
+            padding: {
+                left: 20,
+                right: 20,
+                top: 0,
+                bottom: 20
+            }
+        },
+        legend: {
+          reverse: true,
+          labels: {
+            boxWidth: 60,
+            fontSize: 15,
+          }
+        }
+    }
+});
+
