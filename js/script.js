0577197316f3763f30303b3a40db1ae15d5698ca
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-09-21 21:47:24 +0200

    Run build task and clean all out of 1000+ errors

--- js/script.js
+++ js/script.js
@@ -45,39 +45,39 @@ initPage();
 /* Modal functions */
 
 function closeModal() {
-  document.getElementById('overlay').classList.remove('show')
+  document.getElementById('overlay').classList.remove('show');
 }
 
 document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
   btn.addEventListener('click', function(e) {
     e.preventDefault();
     closeModal();
-  })
-})
+  });
+});
 
 document.querySelector('#overlay').addEventListener('click', function(e) {
   if(e.target === this) {
-    closeModal()
+    closeModal();
   }
-})
+});
 
 document.addEventListener('keyup', function(e) {
   if (e.key == 'Escape') {
-    closeModal()
+    closeModal();
   }
-})
+});
 
 function modalOpener(modal) {
   document.querySelectorAll('#overlay > *').forEach(function(modal) {
-    modal.classList.remove('show')
-  })
-  document.querySelector('#overlay').classList.add('show')
-  document.querySelector(modal).classList.add('show')
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
 }
 
 document.querySelector('.sidebar-manager').addEventListener('click', function(){
-  modalOpener('#myModal')
-})
+  modalOpener('#myModal');
+});
 
 
 /* Sidebar functions */
@@ -94,7 +94,7 @@ function openCloseSidebar() {
     sidebar.style.display = 'block';
   }
 }
-
+// eslint-disable-next-line no-unused-vars
 function showSidebar() {
   let size = document.body.clientWidth;
   if (size >= 737) {
@@ -129,54 +129,55 @@ function sidebarListeners() {
 }
 sidebarListeners();
 
-const searchInput = document.querySelector('.search-input')
+const searchInput = document.querySelector('.search-input');
 searchInput.addEventListener('focus', function() {
-document.querySelector('.icon-message').classList.add('hide')
-})
+  document.querySelector('.icon-message').classList.add('hide');
+});
 
 /* Chart */
 
 let ctx = document.getElementById('myChart').getContext('2d');
+// eslint-disable-next-line
 let chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-        datasets: [{
-            label: "Signups",
-            backgroundColor: '#8DBEC8',
-            borderColor: '#8DBEC8',
-            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
-        },
-        {
-            label: "FTD",
-            backgroundColor: '#F29E4E',
-            borderColor: '#F29E4E',
-            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
-        },
-        {
-            label: "Earned",
-            backgroundColor: '#71B374',
-            borderColor: '#71B374',
-            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-
-        }]
+  type: 'bar',
+  data: {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    datasets: [{
+      label: 'Signups',
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
     },
-    options: {
-        layout: {
-            padding: {
-                left: 20,
-                right: 20,
-                top: 0,
-                bottom: 20
-            }
-        },
-        legend: {
-          reverse: true,
-        labels: {
-          boxWidth: 60,
-          fontSize: 15,
-        }
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+
+    }]
+  },
+  options: {
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 0,
+        bottom: 20
+      }
+    },
+    legend: {
+      reverse: true,
+      labels: {
+        boxWidth: 60,
+        fontSize: 15,
       }
     }
+  }
 });
 
